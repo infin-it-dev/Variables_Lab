dd3b7c268b369ca1465985543d16b97873387ef6
DataGamingYT <rylanhughey@gmail.com>
2022-09-09 09:16:23 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Variables
+{
+    public static void main ( String[] args )
+    {
+        //define 1 variable of each of the
+        //following data types
+        //byte	short		int 		long
+        //float	double
+        //char      boolean	String
+
+        //integer variables
+        byte byteOne = 127;
+
+        System.out.println("/////////////////////////////////");
+        System.out.println("*Some Person            07/18/22*");
+        System.out.println("*                               *");
+        System.out.println("*        integer types          *");
+        System.out.println("*                               *");
+        System.out.println("*8 bit - byteOne = "+byteOne+"\t\t*");
+    }
+}
